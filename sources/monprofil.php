98afd7e16c9611c86df8c9d9d6e8b7ea50bebf97
Thomas SAUSSOL <thomas.saussol@orange.fr>
2020-02-20 11:26:56 +0100

    petites modifications

--- sources/monprofil.php
+++ sources/monprofil.php
@@ -4,14 +4,14 @@ session_start();
 if(isset($_POST['deco']))
 {
 unset($_SESSION['login']);
-unset($_SESSION['password']);	
+unset($_SESSION['password']);
 }
 
 
 if(isset($_SESSION['login']))
 {
 ?>
-<html>
+<html> 
 
 <head>
 		<link href="camping.css" rel="stylesheet">
@@ -19,7 +19,7 @@ if(isset($_SESSION['login']))
 </head>
 
 <body class="profil">
-<header>	
+<header>
 	<ul>
 		<li><a href="../index.php">Accueil</a></li>
 		<div class="deco">
@@ -33,22 +33,22 @@ if(isset($_SESSION['login']))
 <?php
 $login=$_SESSION['login'];
 $password=$_SESSION['password'];
-$connexion= mysqli_connect("localhost", "root", "", "camping"); 	
+$connexion= mysqli_connect("localhost", "root", "", "camping");
 $query="SELECT *from utilisateurs WHERE login='$login'";
 $result= mysqli_query($connexion, $query);
 $row = mysqli_fetch_array($result);
-	
+
 if(isset($_POST['modifier']))
 {
-		
+
 $loginexistant=false;
 $modif=false;
-$connexion= mysqli_connect("localhost", "root", "", "camping"); 
+$connexion= mysqli_connect("localhost", "root", "", "camping");
 $login=$_POST['login'];
 $checkdups="SELECT  *from utilisateurs WHERE login='$login'";
 $checkdups2=mysqli_query($connexion, $checkdups) or die(mysqli_error($connexion));
 $checkdups3=mysqli_num_rows($checkdups2);
-{	
+{
 
 
 if (($_POST['login'] !=  $row['login'])&&($checkdups3 == 0)&&(strlen($_POST['password']) > 5))
@@ -57,17 +57,17 @@ if (($_POST['login'] !=  $row['login'])&&($checkdups3 == 0)&&(strlen($_POST['pas
 	$result= mysqli_query($connexion, $query);
 	$_SESSION['login']=$_POST['login'];
 	$modif=true;
-	mysqli_close($connexion);	
+	mysqli_close($connexion);
 }
 else if (($checkdups3 > 0)&&($_POST['login'] !=  $row['login']))
-{ 	
+{
 	$loginexistant=true;
 	$modif=false;
 	?>
 	<div class="erreur">
 	<div class="affichage">
 	<?php
-	echo "*Login déjà existant";	
+	echo "*Login déjà existant";
 	?>
 	</div>
 	</div>
@@ -80,19 +80,19 @@ else if (strlen($_POST['password']) < 5)
 	<div class="erreur">
 	<div class="affichage">
 	<?php
-	echo "*Mot de passe trop court";	
+	echo "*Mot de passe trop court";
 	?>
 	</div>
 	</div>
-	<?php	
-	
+	<?php
+
 }
 
 
 if (($_POST['password'] !=  $password)&&($loginexistant==false)&&(strlen($_POST['password']) > 5))
 {
-	$hash = password_hash($_POST['password'], PASSWORD_BCRYPT, ['cost' => 12]);	
-	$connexion= mysqli_connect("localhost", "root", "", "camping"); 	
+	$hash = password_hash($_POST['password'], PASSWORD_BCRYPT, ['cost' => 12]);
+	$connexion= mysqli_connect("localhost", "root", "", "camping");
 	$query="UPDATE `utilisateurs` SET `password` = '".$hash."' WHERE `utilisateurs`.`password`='".$row['password']."'";
 	$result= mysqli_query($connexion, $query);
     $_SESSION['password']=$_POST['password'];
@@ -111,7 +111,7 @@ else if ($modif==false)
 	<div class="erreur">
 	<div class="affichage">
 	<?php
-	echo "*Aucune modification a été faite";	
+	echo "*Aucune modification a été faite";
 	?>
 	</div>
 	</div>
@@ -124,9 +124,9 @@ else if ($modif==false)
 <section>
 	<div class="form-style-5">
 	<form class="form" method="post" action="monprofil.php">
-		<legend><span class="rond"></span>Profil</legend>	
-		<input placeholder="Login" required name="login" type="text" id="meeting-time" value="<?php echo $row['login'] ?>">	
-		<input placeholder="Password  (5 caractères minimum)" required name="password" type="password" id="meeting-time" value="<?php echo $password ?>">				
+		<legend><span class="rond"></span>Profil</legend>
+		<input placeholder="Login" required name="login" type="text" id="meeting-time" value="<?php echo $row['login'] ?>">
+		<input placeholder="Password  (5 caractères minimum)" required name="password" type="password" id="meeting-time" value="<?php echo $password ?>">
 		<input name="modifier" type="submit" value="Modifier" />
 	</form>
 </div>
@@ -138,6 +138,6 @@ else if ($modif==false)
 }
 else
 {
-header ('location: connexion.php');	
-}	
+header ('location: connexion.php');
+}
 ?>
